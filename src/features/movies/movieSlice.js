1b85955f9430f5c50e1f0dfded3fef5839aa5ca2
Mustafa Çoban <mstf.cobann1@gmail.com>
2023-09-08 18:55:00 +0300

    movie list,show and detail

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,19 +1,99 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import movieApi from '../../common/apis/movieApi';
+import { APIKey } from "../../common/apis/movieApiKey";
 
-const initialState = {
-    movies: {}
-}
+export const fetchAsyncMovies = createAsyncThunk(`movies/fetchAsyncMovies`, async () => {
+    const movieText = "Harry"
+    const response = await movieApi
+            .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
 
-const movieSlice = createSlice ({
-    name: "movies",
-    initialState,
-    reducers: {
-        addMovies: (state, {payload}) => {
-            state.movies = payload;           
-        }
-    }
+        return response.data;
 })
 
+export const fetchAsyncShows = createAsyncThunk(`movies/fetchAsyncShows`, async () => {
+    const seriesText = "Friends"
+    const response = await movieApi
+            .get(`?apiKey=${APIKey}&s=${seriesText}&type=series`)
+
+        return response.data;
+})
+
+export const fetchAsyncMovieOrShowDetail = createAsyncThunk("movies/fetchAsyncMovieOrShowDetail", async (id) => {
+    const response = await movieApi
+            .get(`?apiKey=${APIKey}&i=${id}$plot=full`)
+
+        return response.data;
+})
+
+const initialState = {
+    movies: {},
+    shows: {},
+    selectMovieOrShow: {},
+}
+
+// const movieSlice = createSlice ({
+//     name: "movies",
+//     initialState,
+//     reducers: {
+//         addMovies: (state, {payload}) => {
+//             state.movies = payload;           
+//         }
+//     },
+//     extraReducers: {
+//         [fetchAsyncMovies.pending] : () => {
+//             console.log("Pending")
+//         },
+//         [fetchAsyncMovies.fulfilled] : (state, {payload}) => {
+//             console.log("Fetched Successfully");
+//             return {...state, movies: payload};
+//         },
+//         [fetchAsyncMovies.rejected] : () => {
+//             console.log("Rejected");
+//         },
+//         [fetchAsyncShows.fulfilled] : (state, {payload}) => {
+//             console.log("Fetched Successfully");
+//             return {...state, shows: payload};
+//         },
+//         [fetchAsyncMovieOrShowDetail.fulfilled] : (state, {payload}) => {
+//             console.log("Fetched Successfully");
+//             return {...state, selectMovieOrShow: payload};
+//         },
+//     },
+    
+
+    const movieSlice = createSlice({
+        name: "movies",
+        initialState,
+        reducers: {
+          addMovies: (state, { payload }) => {
+            state.movies = payload;
+          },
+        },
+        extraReducers: (builder) => {
+          builder
+            .addCase(fetchAsyncMovies.pending, (state) => {
+              console.log("Pending");
+            })
+            .addCase(fetchAsyncMovies.fulfilled, (state, { payload }) => {
+              console.log("Fetched Successfully");
+              state.movies = payload;
+            })
+            .addCase(fetchAsyncMovies.rejected, (state) => {
+              console.log("Rejected");
+            })
+            .addCase(fetchAsyncShows.fulfilled, (state, { payload }) => {
+              console.log("Fetched Successfully");
+              state.shows = payload;
+            })
+            .addCase(fetchAsyncMovieOrShowDetail.fulfilled, (state, { payload }) => {
+              console.log("Fetched Successfully");
+              state.selectMovieOrShow = payload;
+            });
+        },
+      });
+
 export const { addMovies } = movieSlice.actions;
-export const getAllMovies = (state) => state.movies.movies
-export default movieSlice.reducer
+export const getAllMovies = (state) => state.movies.movies;
+export const getAllShows = (state) => state.movies.shows;
+export const getSelectedMovieOrShow = (state) => state.movies.selectMovieOrShow;
+export default movieSlice.reducer;
